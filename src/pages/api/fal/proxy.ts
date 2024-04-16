11d3ddbbc288407f1f5fdf847457f36206a355af
luc <luc@forgotten.name>
2024-04-15 21:47:46 -0700

    Fixed fal proxy api route

--- src/pages/api/fal/proxy.ts
+++ src/pages/api/fal/proxy.ts
@@ -1,5 +1,25 @@
-export { handler as default } from '@fal-ai/serverless-proxy/nextjs';
+import { NextRequest, NextResponse } from 'next/server';
+import { route } from "@fal-ai/serverless-proxy/nextjs";
 
 export const config = {
     runtime: 'edge',
 };
+
+export const POST = (req: NextRequest) => { 
+  return route.POST(req);
+};
+
+export const GET = route.GET;
+
+// Default export function
+export default function handler(req: NextRequest) {
+  switch (req.method) {
+    case 'GET':
+      return GET(req);
+    case 'POST':
+      return POST(req);
+    default:
+      // Optionally handle other methods or return a 405 Method Not Allowed response
+      return new NextResponse('Method Not Allowed', { status: 405 });
+  }
+}
